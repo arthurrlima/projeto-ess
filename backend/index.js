8a17c3473dbcb41b9fceefea5226c34eea3bedd1
arthurrlima <arl3@cin.ufpe.br>
2024-06-18 09:41:31 -0300

    update index.js with acommodations and reservations routes

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,12 @@ import pingRoutes from './routes/ping.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import paymentMethodsRoutes from './routes/paymentMethods.routes.js';
 import acomRoutes from './routes/acom.routes.js';
+import reservationRoutes from '/.routes/reservation.routes.js';
 
 app.use('/ping', pingRoutes);
 app.use('/auth', authRoutes);
 app.use('/payment-methods', paymentMethodsRoutes);
 app.use('/host/accommodations', acomRoutes);
-
+app.use('/host/accomodations/reservations', reservationRoutes)
 
 app.listen(5001);
